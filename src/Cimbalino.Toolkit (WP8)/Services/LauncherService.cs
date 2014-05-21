01ce94cb6de58b4748219c2cbbf236ae35268173
Pedro Lamas <pedrolamas@gmail.com>
2014-05-21 16:04:54 +0100

    Added the ILauncherService interface and implementation

---
+++ src/Cimbalino.Toolkit (WP8)/Services/LauncherService.cs
@@ -0,0 +1,49 @@
+﻿// ****************************************************************************
+// <copyright file="LauncherService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.System;
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="ILauncherService"/>.
+    /// </summary>
+    public class LauncherService : ILauncherService
+    {
+        /// <summary>
+        /// Starts the default app associated with the URI scheme name for the specified <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="uri">The <see cref="Uri"/> to start.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public async Task LaunchUriAsync(Uri uri)
+        {
+            await Launcher.LaunchUriAsync(uri);
+        }
+
+        /// <summary>
+        /// Starts the default app associated with the specified file.
+        /// </summary>
+        /// <param name="file">The file to start.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public async Task LaunchFileAsync(string file)
+        {
+            var storageFile = await ApplicationData.Current.LocalFolder.GetFileAsync(file);
+
+            await Launcher.LaunchFileAsync(storageFile);
+        }
+    }
+}
---
+++ src/Cimbalino.Toolkit.Portable/Services/ILauncherService.cs
@@ -0,0 +1,39 @@
+﻿// ****************************************************************************
+// <copyright file="ILauncherService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Portable</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Threading.Tasks;
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of starting the default app associated with the specified file or <see cref="Uri"/>.
+    /// </summary>
+    public interface ILauncherService
+    {
+        /// <summary>
+        /// Starts the default app associated with the URI scheme name for the specified <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="uri">The <see cref="Uri"/> to start.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task LaunchUriAsync(Uri uri);
+
+        /// <summary>
+        /// Starts the default app associated with the specified file.
+        /// </summary>
+        /// <param name="file">The file to start.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task LaunchFileAsync(string file);
+    }
+}
