c1227eef6f2c2590f7411a690a2afefb5ef52355
Pedro Lamas <pedrolamas@gmail.com>
2014-06-04 21:44:22 +0100

    Added the IDeviceSettingsService interface and implementation

---
+++ src/Cimbalino.Toolkit (WP8)/Services/DeviceSettingsService.cs
@@ -0,0 +1,82 @@
+﻿// ****************************************************************************
+// <copyright file="DeviceSettingsService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if WINDOWS_PHONE || WINDOWS_PHONE_APP
+using System;
+using System.Threading.Tasks;
+using Windows.System;
+#else
+using System;
+using System.Threading.Tasks;
+#endif
+
+namespace Cimbalino.Toolkit.Services
+{
+    public class DeviceSettingsService : IDeviceSettingsService
+    {
+        public async Task ShowAirplaneModeSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-airplanemode:");
+        }
+
+        public async Task ShowBluetoothSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-bluetooth:");
+        }
+
+        public async Task ShowCellularSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-cellular:");
+        }
+
+        public async Task ShowEmailAndAccountsSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-emailandaccounts:");
+        }
+
+        public async Task ShowLocationSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-location:");
+        }
+
+        public async Task ShowLockSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-lock:");
+        }
+
+        public async Task ShowPowerSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-power:");
+        }
+
+        public async Task ShowScreenRotationSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-screenrotation:");
+        }
+
+        public async Task ShowWiFiSettingsAsync()
+        {
+            await LaunchUrlAsync("ms-settings-wifi:");
+        }
+
+        private async Task LaunchUrlAsync(string url)
+        {
+#if WINDOWS_PHONE || WINDOWS_PHONE_APP
+            await Launcher.LaunchUriAsync(new Uri(url, UriKind.Absolute));
+#else
+            throw new NotSupportedException();
+#endif
+        }
+    }
+}
---
+++ src/Cimbalino.Toolkit.Portable/Services/IDeviceSettingsService.cs
@@ -0,0 +1,39 @@
+﻿// ****************************************************************************
+// <copyright file="IDeviceSettingsService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System.Threading.Tasks;
+
+namespace Cimbalino.Toolkit.Services
+{
+    public interface IDeviceSettingsService
+    {
+        Task ShowAirplaneModeSettingsAsync();
+        
+        Task ShowBluetoothSettingsAsync();
+
+        Task ShowCellularSettingsAsync();
+
+        Task ShowEmailAndAccountsSettingsAsync();
+
+        Task ShowLocationSettingsAsync();
+        
+        Task ShowLockSettingsAsync();
+        
+        Task ShowPowerSettingsAsync();
+        
+        Task ShowScreenRotationSettingsAsync();
+
+        Task ShowWiFiSettingsAsync();
+    }
+}
