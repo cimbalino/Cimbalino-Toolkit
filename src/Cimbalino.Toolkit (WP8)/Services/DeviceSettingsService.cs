bafa13429e8275e6ae77c464bb9cb3ceb5ba86ec
Pedro Lamas <pedrolamas@gmail.com>
2014-06-04 21:56:46 +0100

    Added the IDeviceSettingsService documentation

--- src/Cimbalino.Toolkit (WP8)/Services/DeviceSettingsService.cs
+++ src/Cimbalino.Toolkit (WP8)/Services/DeviceSettingsService.cs
@@ -23,48 +23,87 @@ using System.Threading.Tasks;
 
 namespace Cimbalino.Toolkit.Services
 {
+    /// <summary>
+    /// Represents an implementation of the <see cref="IDeviceSettingsService"/>.
+    /// </summary>
     public class DeviceSettingsService : IDeviceSettingsService
     {
+        /// <summary>
+        /// Shows the Airplane Mode settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowAirplaneModeSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-airplanemode:");
         }
 
+        /// <summary>
+        /// Shows the Bluetooth settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowBluetoothSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-bluetooth:");
         }
 
+        /// <summary>
+        /// Shows the Cellular settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowCellularSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-cellular:");
         }
 
+        /// <summary>
+        /// Shows the Email and Accounts settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowEmailAndAccountsSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-emailandaccounts:");
         }
 
+        /// <summary>
+        /// Shows the Location settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowLocationSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-location:");
         }
 
-        public async Task ShowLockSettingsAsync()
+        /// <summary>
+        /// Shows the Lock Screen settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public async Task ShowLockScreenSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-lock:");
         }
 
+        /// <summary>
+        /// Shows the Power settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowPowerSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-power:");
         }
 
+        /// <summary>
+        /// Shows the Screen Rotation settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowScreenRotationSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-screenrotation:");
         }
 
+        /// <summary>
+        /// Shows the Wi-Fi settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public async Task ShowWiFiSettingsAsync()
         {
             await LaunchUrlAsync("ms-settings-wifi:");
--- src/Cimbalino.Toolkit.Portable/Services/IDeviceSettingsService.cs
+++ src/Cimbalino.Toolkit.Portable/Services/IDeviceSettingsService.cs
@@ -16,24 +16,63 @@ using System.Threading.Tasks;
 
 namespace Cimbalino.Toolkit.Services
 {
+    /// <summary>
+    /// Represents a service capable of launching a Settings dialog that allows the user to change the device's settings.
+    /// </summary>
     public interface IDeviceSettingsService
     {
+        /// <summary>
+        /// Shows the Airplane Mode settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowAirplaneModeSettingsAsync();
-        
+
+        /// <summary>
+        /// Shows the Bluetooth settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowBluetoothSettingsAsync();
 
+        /// <summary>
+        /// Shows the Cellular settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowCellularSettingsAsync();
 
+        /// <summary>
+        /// Shows the Email and Accounts settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowEmailAndAccountsSettingsAsync();
 
+        /// <summary>
+        /// Shows the Location settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowLocationSettingsAsync();
-        
-        Task ShowLockSettingsAsync();
-        
+
+        /// <summary>
+        /// Shows the Lock Screen settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task ShowLockScreenSettingsAsync();
+
+        /// <summary>
+        /// Shows the Power settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowPowerSettingsAsync();
-        
+
+        /// <summary>
+        /// Shows the Screen Rotation settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowScreenRotationSettingsAsync();
 
+        /// <summary>
+        /// Shows the Wi-Fi settings dialog.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         Task ShowWiFiSettingsAsync();
     }
 }
