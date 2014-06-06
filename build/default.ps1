34a511d1cbc17e9ec8d129551bbea9a982df893e
Pedro Lamas <pedrolamas@gmail.com>
2014-06-06 16:21:03 +0100

    Preparing the solution for automated build

--- build/default.ps1
+++ build/default.ps1
@@ -5,7 +5,7 @@ properties {
   $toolsDir = "$baseDir\tools"
   $binDir = "$baseDir\bin"
   
-  $version = "4.0.0"
+  $version = "1.0.0-beta1"
   
   $tempDir = "$binDir\temp"
   $binariesDir = "$binDir\binaries"
@@ -16,22 +16,21 @@ properties {
   $7zip = "$toolsDir\7zip\7za.exe"
   $vstest = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
   
+  $configurations = @{
+    "Portable" = @{Suffix = ""; Folder="portable-net45+wp8+win8+wpa81"};
+    "WP8" = @{Suffix = " (WP8)"; Folder="wp8"};
+    "WPA81" = @{Suffix = " (WPA81)"; Folder="wpa81"};
+    "Win81" = @{Suffix = " (Win81)"; Folder="win81"}
+  }
+  
   $projects = @(
-    @{Name = "Cimbalino.Toolkit.Portable"},
-    @{Name = "Cimbalino.Toolkit (WP8)"},
-    @{Name = "Cimbalino.Toolkit (WP81)"},
-    @{Name = "Cimbalino.Toolkit (Win81)"},
-    @{Name = "Cimbalino.Toolkit.Background (WP8)"},
-    @{Name = "Cimbalino.Toolkit.Background (WP81)"},
-    @{Name = "Cimbalino.Toolkit.Background (Win81)"}
-  )
-  $configurations = @(
-    @{Name = "WP71"; Folder = "sl4-wp71"},
-    @{Name = "WP8"; Folder = "wp8"}
+    @{Name = "Cimbalino.Toolkit.Portable"; Configurations = @("Portable")},
+    @{Name = "Cimbalino.Toolkit"; Configurations = @("WP8", "WPA81", "Win81")},
+    @{Name = "Cimbalino.Toolkit.Background"; Configurations = @("WP8", "WPA81", "Win81")}
   )
 }
 
-Framework "4.0x86"
+Framework "4.5.1x86"
 
 task default -depends ?
 
@@ -56,20 +55,25 @@ task Headers -description "Updates the headers in *.cs files" {
 
   $projects | % {
     $projectName = $_.Name
-    $project = ($projectName -Replace "\s\(.*$", "")
-    $fullProjectName = "$projectName"
-    $projectDir = "$sourceDir\$fullProjectName\"
     
-    Get-ChildItem -Path $projectDir -Filter *.cs -Exclude *generated* -Recurse | % {
-      $fullFilename = $_.FullName
-      $filename = $_.Name
+    $_.Configurations | % {
+      $configuration = $configurations[$_]
+      $configurationSuffix = $configuration.Suffix
+      $fullProjectName = "$projectName$configurationSuffix"
       
-      $oldContent = [System.IO.File]::ReadAllText($fullFilename)
+      $projectDir = "$sourceDir\$fullProjectName\"
       
-      $newContent = ($headerTemplate -f $project, $filename) + ($oldContent -Replace "(?s)^.*// \*+\r\n\r\n", "")
-      
-      if ($newContent -ne $oldContent) {
-        [System.IO.File]::WriteAllText($fullFilename, $newContent, [System.Text.Encoding]::UTF8)
+      Get-ChildItem -Path $projectDir -Filter *.cs -Exclude *generated* -Recurse | % {
+        $fullFilename = $_.FullName
+        $filename = $_.Name
+        
+        $oldContent = [System.IO.File]::ReadAllText($fullFilename)
+        
+        $newContent = ($headerTemplate -f $projectName, $filename) + ($oldContent -Replace "(?s)^.*// \*+\r\n\r\n", "")
+        
+        if ($newContent -ne $oldContent) {
+          [System.IO.File]::WriteAllText($fullFilename, $newContent, [System.Text.Encoding]::UTF8)
+        }
       }
     }
   }
@@ -83,12 +87,14 @@ task Version -description "Updates the version entries in AssemblyInfo.cs files"
   $assemblyVersionValue = 'AssemblyVersion("' + $assemblyVersion + '")';
   $fileVersionValue = 'AssemblyFileVersion("' + $fileVersion + '")';
   
-  $configurations | % {
-    $configName = $_.Name
-
-    $projects | % {
-      $projectName = $_.Name
-      $fullProjectName = "$projectName ($configName)"
+  $projects | % {
+    $projectName = $_.Name
+    
+    $_.Configurations | % {
+      $configuration = $configurations[$_]
+      $configurationSuffix = $configuration.Suffix
+      $fullProjectName = "$projectName$configurationSuffix"
+      
       $projectDir = "$sourceDir\$fullProjectName\"
       
       Write-Host -ForegroundColor Green "Versioning $fullProjectName..."
@@ -97,12 +103,101 @@ task Version -description "Updates the version entries in AssemblyInfo.cs files"
       Get-ChildItem -Path $projectDir -Filter AssemblyInfo.cs -Recurse | % {
         $fullFilename = $_.FullName
         
-        (Get-Content -Path $fullFilename) | % {
-          % { $_ -Replace $assemblyVersionPattern, $assemblyVersionValue } |
-          % { $_ -Replace $fileVersionPattern, $fileVersionValue }
-        } | Set-Content -Path $fullFilename -Encoding UTF8
+        $oldContent = [System.IO.File]::ReadAllText($fullFilename);
+        
+        $newContent = $oldContent -Replace $assemblyVersionPattern, $assemblyVersionValue -Replace $fileVersionPattern, $fileVersionValue
+        
+        if ($newContent -ne $oldContent) {
+          [System.IO.File]::WriteAllText($fullFilename, $newContent, [System.Text.Encoding]::UTF8)
+        }
       }
     }
+  }
+}
+
+task Build -depends Clean, Setup, Version -description "Build all projects and get the assemblies" {
+  $tempBinariesDir = "$tempDir\binaries"
+  
+  New-Item -Path $binariesDir -ItemType Directory | Out-Null
+  New-Item -Path $tempBinariesDir -ItemType Directory | Out-Null
+  
+  Exec { msbuild "/t:Clean;Build" /p:Configuration=Release /p:OutDir=$tempBinariesDir /p:GenerateProjectSpecificOutputFolder=true /p:StyleCopTreatErrorsAsWarnings=true /m "$sourceDir\Cimbalino.Toolkit.sln" } "Error building $solutionFile"
+  
+  $projects | % {
+    $projectName = $_.Name
+    
+    $_.Configurations | % {
+      $configuration = $configurations[$_]
+      $configurationSuffix = $configuration.Suffix
+      $configurationFolder = $configuration.Folder
+      $fullProjectName = "$projectName$configurationSuffix"
+      
+      $projectDir = "$binariesDir\$projectName"
+      $configurationDir = "$projectDir\$configurationFolder"
+    
+      New-Item -Path $configurationDir -ItemType Directory | Out-Null
+      
+      Copy-Item -Path $tempBinariesDir\$fullProjectName\$projectName.* -Destination $configurationDir\ -Recurse
+    }
+  }
+}
+
+task PackNuGet -depends Build -description "Create the NuGet packages" {
+  $tempNupkgDir = "$tempDir\nupkg"
+  
+  New-Item -Path $nupkgDir -ItemType Directory | Out-Null
+  New-Item -Path $tempNupkgDir -ItemType Directory | Out-Null
+  
+  $projects | % {
+    $projectName = $_.Name
+    $projectNugetFolder = "$tempNupkgDir\$projectName"
+    $projectNuspec = "$projectName.nuspec"
+    
+    New-Item -Path $projectNugetFolder -ItemType Directory | Out-Null
+    
+    (Get-Content -Path $buildDir\$projectNuspec) | % {
+          % { $_ -Replace '\$version\$', $version }
+        } | Set-Content -Path $projectNugetFolder\$projectNuspec -Encoding UTF8
+    
+    $projectBuildFolder = "$projectNugetFolder\build"
+    $projectLibFolder = "$projectNugetFolder\lib"
+    
+    $_.Configurations | % {
+      $configurationName = $_
+      $configuration = $configurations[$_]
+      $configurationFolder = $configuration.Folder
+      
+      $projectTargets = "$buildDir\$configurationName\$projectName.targets"
+      
+      if (Test-Path $projectTargets)
+      {
+        $projectBuildConfigurationFolder = "$projectBuildFolder\$configurationFolder"
+        
+        New-Item -Path $projectBuildConfigurationFolder -ItemType Directory | Out-Null
+        
+        Copy-Item -Path $projectTargets -Destination $projectBuildConfigurationFolder\ -Recurse
+      }
+    }
+    
+    New-Item -Path $projectLibFolder -ItemType Directory | Out-Null
+    
+    Copy-Item -Path $binariesDir\$projectName\* -Destination $projectLibFolder\ -Recurse
+    
+    Write-Host -ForegroundColor Green "Packaging $projectName..."
+    Write-Host
+    
+    Exec { .$nuget pack $projectNugetFolder\$projectNuspec -Output $nupkgDir\ } "Error packaging $name"
+  }
+}
+
+task PublishNuget -depends PackNuGet -description "Publish the NuGet packages to the remote repositories" {
+  Get-ChildItem $nupkgDir\*.nupkg | % {
+    $nupkg = $_.FullName
+    
+    Write-Host -ForegroundColor Green "Publishing $nupkg..."
+    Write-Host
+    
+    Exec { .$nuget push $nupkg } "Error publishing $nupkg"
   }
 }
 
