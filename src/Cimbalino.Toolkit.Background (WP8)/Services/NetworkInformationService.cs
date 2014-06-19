465986c10e05763c79966596f018583cb5a3f2b1
Sara Silva <saramgsilva@gmail.com>
2014-06-19 21:17:40 +0100

    Add minimal INetworkInformationService and its implementation.

---
+++ src/Cimbalino.Toolkit.Background (WP8)/Services/NetworkInformationService.cs
@@ -0,0 +1,38 @@
+﻿#if WINDOWS_PHONE
+using Microsoft.Phone.Net.NetworkInformation;
+#endif
+
+using System;
+using Windows.Networking.Connectivity;
+
+namespace Cimbalino.Toolkit.Services
+{
+    public class NetworkInformationService : INetworkInformationService
+    {
+        public NetworkInformationService()
+        {
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+        }
+
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            OnNetworkStatusChanged(sender);
+        }
+
+        public void OnNetworkStatusChanged(object sender)
+        {
+            if (NetworkStatusChanged != null)
+            {
+                OnNetworkStatusChanged(sender);
+            }
+        }
+
+        public event NetworkStatusChangedEventHandler NetworkStatusChanged;
+
+        public bool IsNetworkAvailable 
+        {
+            get { return NetworkInformation.GetInternetConnectionProfile() != null; }
+        }
+    }
+
+}
---
+++ src/Cimbalino.Toolkit.Portable/Services/INetworkInformationService.cs
@@ -0,0 +1,10 @@
+﻿namespace Cimbalino.Toolkit.Services
+{
+    public delegate void NetworkStatusChangedEventHandler(object sender);
+
+    public interface INetworkInformationService
+    {
+        event NetworkStatusChangedEventHandler NetworkStatusChanged;
+        bool IsNetworkAvailable { get;}
+    }
+}
