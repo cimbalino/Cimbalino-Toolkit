42a94132662e51f260dcff72bc59ee8f61a2d4f5
Pedro Lamas <pedrolamas@gmail.com>
2014-06-27 14:24:53 +0100

    Added the IMapManagerService interface and implementation

---
+++ src/Cimbalino.Toolkit (WP8)/Services/MapManagerService.cs
@@ -0,0 +1,58 @@
+﻿// ****************************************************************************
+// <copyright file="MapManagerService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if WINDOWS_PHONE
+using Microsoft.Phone.Tasks;
+#elif WINDOWS_PHONE_APP
+using Windows.Services.Maps;
+#else
+using System;
+#endif
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="IMapManagerService"/>.
+    /// </summary>
+    public class MapManagerService : IMapManagerService
+    {
+        /// <summary>
+        /// Displays the UI that lets users download maps for offline use.
+        /// </summary>
+        public void ShowDownloadedMapsUI()
+        {
+#if WINDOWS_PHONE
+            new MapDownloaderTask().Show();
+#elif WINDOWS_PHONE_APP
+            MapManager.ShowDownloadedMapsUI();
+#else
+            throw new NotSupportedException();
+#endif
+        }
+
+        /// <summary>
+        /// Displays the UI that lets users update maps that were previously downloaded for offline use.
+        /// </summary>
+        public void ShowMapsUpdateUI()
+        {
+#if WINDOWS_PHONE
+            new MapUpdaterTask().Show();
+#elif WINDOWS_PHONE_APP
+            MapManager.ShowMapsUpdateUI();
+#else
+            throw new NotSupportedException();
+#endif
+        }
+    }
+}
---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/IMapManagerService.cs
@@ -0,0 +1,32 @@
+﻿// ****************************************************************************
+// <copyright file="IMapManagerService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Core</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of displaying the UI that lets users download maps for offline use, or update maps that were previously downloaded.
+    /// </summary>
+    public interface IMapManagerService
+    {
+        /// <summary>
+        /// Displays the UI that lets users download maps for offline use.
+        /// </summary>
+        void ShowDownloadedMapsUI();
+
+        /// <summary>
+        /// Displays the UI that lets users update maps that were previously downloaded for offline use.
+        /// </summary>
+        void ShowMapsUpdateUI();
+    }
+}
