1a354e724b23b841f41f2af5d959b897f8cc8dfa
Pedro Lamas <pedrolamas@gmail.com>
2014-07-10 10:07:24 +0100

    Added the NamescopeBinding helper class

---
+++ src/Cimbalino.Toolkit (WP8)/Helpers/NamescopeBinding.cs
@@ -0,0 +1,44 @@
+﻿// ****************************************************************************
+// <copyright file="NamescopeBinding.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if WINDOWS_PHONE
+using System.Windows;
+#else
+using Windows.UI.Xaml;
+#endif
+
+namespace Cimbalino.Toolkit.Helpers
+{
+    /// <summary>
+    /// Allows binding of objects in a different XAML namescope.
+    /// </summary>
+    public class NamescopeBinding : DependencyObject
+    {
+        /// <summary>
+        /// Gets or sets the source <see cref="FrameworkElement"/> to bind.
+        /// </summary>
+        /// <value>The source <see cref="FrameworkElement"/> to bind.</value>
+        public FrameworkElement Source
+        {
+            get { return (FrameworkElement)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifier for the <see cref="Source" /> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(FrameworkElement), typeof(NamescopeBinding), null);
+    }
+}
