ec1c747ddb287da96bbe89a0fc74466d37a9060a
Pedro Lamas <pedrolamas@gmail.com>
2014-08-13 21:23:50 +0100

    Added the NegativeBooleanConverter

---
+++ src/Cimbalino.Toolkit (WP8)/Converters/NegativeBooleanConverter.cs
@@ -0,0 +1,60 @@
+﻿// ****************************************************************************
+// <copyright file="NegativeBooleanConverter.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if WINDOWS_PHONE
+using System;
+using System.Windows.Data;
+#else
+using System;
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Cimbalino.Toolkit.Converters
+{
+    /// <summary>
+    /// An <see cref="IValueConverter"/> which negates a <see cref="bool"/> value.
+    /// </summary>
+    public class NegativeBooleanConverter : ValueConverterBase
+    {
+        /// <summary>
+        /// Negates a <see cref="bool"/> value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        /// <summary>
+        /// Negates a <see cref="bool"/> value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
