910574a949aa980a346ce1b91acf9c93d514cc95
Pedro Lamas <pedrolamas@gmail.com>
2014-08-14 14:03:26 +0100

    Bumped the version to 1.2.0

--- build/default.ps1
+++ build/default.ps1
@@ -5,7 +5,7 @@ properties {
   $toolsDir = "$baseDir\tools"
   $binDir = "$baseDir\bin"
   
-  $version = "1.1.1"
+  $version = "1.2.0"
   
   $tempDir = "$binDir\temp"
   $binariesDir = "$binDir\binaries"
