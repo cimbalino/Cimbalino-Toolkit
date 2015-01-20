e0e04d9cb044e00de60d3a15934b658e579af6f0
ScottIsAFool <scottisafool@live.co.uk>
2015-01-20 14:41:33 +0000

    Added ICameraInfoService

---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/ICameraInfoService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Cimbalino.Toolkit.Services
+{
+    public interface ICameraInfoService
+    {
+        Task<bool> HasPrimaryCamera();
+        Task<bool> HasFrontFacingCamera();
+        Task<bool> HasFlash();
+        Task<bool> SupportsFocus();
+    }
+}
