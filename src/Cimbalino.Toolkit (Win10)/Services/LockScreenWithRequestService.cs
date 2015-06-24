85b9e70e895ab6eb48230362b85485819bba5e15
Scott Lovegrove <scottisafool@live.co.uk>
2015-06-24 18:26:21 +0100

    Add headers to files in Toolkit projects.

--- src/Cimbalino.Toolkit (Win10)/Services/LockScreenWithRequestService.cs
+++ src/Cimbalino.Toolkit (Win10)/Services/LockScreenWithRequestService.cs
@@ -1,11 +1,11 @@
 ﻿// ****************************************************************************
 // <copyright file="LockScreenWithRequestService.cs" company="Pedro Lamas">
-// Copyright © Pedro Lamas 2015
+// Copyright © Pedro Lamas 2014
 // </copyright>
 // ****************************************************************************
 // <author>Pedro Lamas</author>
 // <email>pedrolamas@gmail.com</email>
-// <project>Cimbalino.Toolkit.Core</project>
+// <project>Cimbalino.Toolkit</project>
 // <web>http://www.pedrolamas.com</web>
 // <license>
 // See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
--- src/Cimbalino.Toolkit (Win10)/Services/TitleBarService.cs
+++ src/Cimbalino.Toolkit (Win10)/Services/TitleBarService.cs
@@ -1,4 +1,17 @@
-﻿#if !WINDOWS_UAP
+﻿// ****************************************************************************
+// <copyright file="TitleBarService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+#if !WINDOWS_UAP
 using System;
 using System.Linq;
 using System.Reflection;
