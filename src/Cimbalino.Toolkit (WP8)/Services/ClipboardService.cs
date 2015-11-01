22cd022dd686fca1c308487e42cbc71ec80b6cad
Pedro Lamas <pedrolamas@gmail.com>
2015-11-01 13:49:31 +0000

    Further improvements and refactoring of the IClipboardService interface and implementations

--- src/Cimbalino.Toolkit (WP8)/Services/ClipboardService.cs
+++ src/Cimbalino.Toolkit (WP8)/Services/ClipboardService.cs
@@ -1,134 +1,183 @@
 ﻿// ****************************************************************************
-// <copyright file="IClipboardService.cs" company="Pedro Lamas">
+// <copyright file="ClipboardService.cs" company="Pedro Lamas">
 // Copyright © Pedro Lamas 2014
 // </copyright>
 // ****************************************************************************
 // <author>Pedro Lamas</author>
 // <email>pedrolamas@gmail.com</email>
-// <project>Cimbalino.Toolkit.Core</project>
+// <project>Cimbalino.Toolkit</project>
 // <web>http://www.pedrolamas.com</web>
 // <license>
 // See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
 // </license>
 // ****************************************************************************
 
+#if WINDOWS_PHONE
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
+using Cimbalino.Toolkit.Helpers;
+#else
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Helpers;
+#endif
 
 namespace Cimbalino.Toolkit.Services
 {
     /// <summary>
-    /// Represents a service capable of handling the system clipboard.
+    /// Represents an implementation of the <see cref="IClipboardService"/>.
     /// </summary>
-    public interface IClipboardService
+    public class ClipboardService : IClipboardService
     {
         /// <summary>
         /// Gets a value indicating whether the clipboard contains text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains text.</value>
-        bool ContainsText { get; }
+        public virtual bool ContainsText { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains a bitmap.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains a bitmap.</value>
-        bool ContainsBitmap { get; }
+        public virtual bool ContainsBitmap { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains HTML text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains HTML text.</value>
-        bool ContainsHtml { get; }
+        public virtual bool ContainsHtml { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains RTF text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains RTF text.</value>
-        bool ContainsRtf { get; }
+        public virtual bool ContainsRtf { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains a web link.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains a web link.</value>
-        bool ContainsWebLink { get; }
+        public virtual bool ContainsWebLink { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains an application link.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains an application link.</value>
-        bool ContainsApplicationLink { get; }
+        public virtual bool ContainsApplicationLink { get; } = ExceptionHelper.ThrowNotSupported<bool>();
 
         /// <summary>
         /// Gets the text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetTextAsync();
+        public virtual Task<string> GetTextAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<string>>();
+        }
 
         /// <summary>
         /// Gets the web link in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Uri> GetWebLinkAsync();
+        public virtual Task<Uri> GetWebLinkAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<Uri>>();
+        }
 
         /// <summary>
         /// Gets the HTML text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetHtmlAsync();
+        public virtual Task<string> GetHtmlAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<string>>();
+        }
 
         /// <summary>
         /// Gets the RTF text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetRtfAsync();
+        public virtual Task<string> GetRtfAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<string>>();
+        }
 
         /// <summary>
         /// Gets the application link in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Uri> GetApplicationLinkAsync();
+        public virtual Task<Uri> GetApplicationLinkAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<Uri>>();
+        }
 
         /// <summary>
         /// Gets the bitmap in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Stream> GetBitmapAsync();
+        public virtual Task<Stream> GetBitmapAsync()
+        {
+            return ExceptionHelper.ThrowNotSupported<Task<Stream>>();
+        }
 
         /// <summary>
         /// Sets the current text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The text content that is stored in the clipboard.</param>
-        void SetText(string content);
+        public virtual void SetText(string content)
+        {
+#if WINDOWS_PHONE
+            Clipboard.SetText(content);
+#else
+            ExceptionHelper.ThrowNotSupported();
+#endif
+        }
 
         /// <summary>
         /// Sets the current web link content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The web link content that is stored in the clipboard.</param>
-        void SetWebLink(Uri content);
+        public virtual void SetWebLink(Uri content)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
 
         /// <summary>
         /// Sets the current application link content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">application link content that is stored in the clipboard.</param>
-        void SetApplicationLink(Uri content);
+        public virtual void SetApplicationLink(Uri content)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
 
         /// <summary>
         /// Sets the current RTF text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The RTF text content that is stored in the clipboard.</param>
-        void SetRtf(string content);
+        public virtual void SetRtf(string content)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
 
         /// <summary>
         /// Sets the current HTML text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The HTML text content that is stored in the clipboard.</param>
-        void SetHtml(string content);
+        public virtual void SetHtml(string content)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
 
         /// <summary>
         /// Sets the current bitmap content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The bitmap content that is stored in the clipboard.</param>
-        void SetBitmap(Stream content);
+        public virtual void SetBitmap(Stream content)
+        {
+            ExceptionHelper.ThrowNotSupported();
+        }
     }
 }
--- src/Cimbalino.Toolkit (Win81)/Services/ClipboardService.cs
+++ src/Cimbalino.Toolkit (Win81)/Services/ClipboardService.cs
@@ -1,11 +1,11 @@
 ﻿// ****************************************************************************
-// <copyright file="IClipboardService.cs" company="Pedro Lamas">
+// <copyright file="ClipboardService.cs" company="Pedro Lamas">
 // Copyright © Pedro Lamas 2014
 // </copyright>
 // ****************************************************************************
 // <author>Pedro Lamas</author>
 // <email>pedrolamas@gmail.com</email>
-// <project>Cimbalino.Toolkit.Core</project>
+// <project>Cimbalino.Toolkit</project>
 // <web>http://www.pedrolamas.com</web>
 // <license>
 // See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
@@ -15,120 +15,207 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage.Streams;
 
 namespace Cimbalino.Toolkit.Services
 {
     /// <summary>
-    /// Represents a service capable of handling the system clipboard.
+    /// Represents an implementation of the <see cref="IClipboardService"/>.
     /// </summary>
-    public interface IClipboardService
+    public class ClipboardService : IClipboardService
     {
         /// <summary>
         /// Gets a value indicating whether the clipboard contains text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains text.</value>
-        bool ContainsText { get; }
+        public virtual bool ContainsText { get; } = IsTypeInClipboard(StandardDataFormats.Text);
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains a bitmap.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains a bitmap.</value>
-        bool ContainsBitmap { get; }
+        public virtual bool ContainsBitmap { get; } = IsTypeInClipboard(StandardDataFormats.Bitmap);
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains HTML text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains HTML text.</value>
-        bool ContainsHtml { get; }
+        public virtual bool ContainsHtml { get; } = IsTypeInClipboard(StandardDataFormats.Html);
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains RTF text.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains RTF text.</value>
-        bool ContainsRtf { get; }
+        public virtual bool ContainsRtf { get; } = IsTypeInClipboard(StandardDataFormats.Rtf);
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains a web link.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains a web link.</value>
-        bool ContainsWebLink { get; }
+        public virtual bool ContainsWebLink { get; } = IsTypeInClipboard(StandardDataFormats.WebLink);
 
         /// <summary>
         /// Gets a value indicating whether the clipboard contains an application link.
         /// </summary>
         /// <value>A value indicating whether the clipboard contains an application link.</value>
-        bool ContainsApplicationLink { get; }
+        public virtual bool ContainsApplicationLink { get; } = IsTypeInClipboard(StandardDataFormats.ApplicationLink);
 
         /// <summary>
         /// Gets the text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetTextAsync();
+        public virtual async Task<string> GetTextAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            return await clipboard.GetTextAsync();
+        }
 
         /// <summary>
         /// Gets the web link in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Uri> GetWebLinkAsync();
+        public virtual async Task<Uri> GetWebLinkAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            return await clipboard.GetWebLinkAsync();
+        }
 
         /// <summary>
         /// Gets the HTML text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetHtmlAsync();
+        public virtual async Task<string> GetHtmlAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            return await clipboard.GetHtmlFormatAsync();
+        }
 
         /// <summary>
         /// Gets the RTF text in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<string> GetRtfAsync();
+        public virtual async Task<string> GetRtfAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            return await clipboard.GetRtfAsync();
+        }
 
         /// <summary>
         /// Gets the application link in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Uri> GetApplicationLinkAsync();
+        public virtual async Task<Uri> GetApplicationLinkAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            return await clipboard.GetApplicationLinkAsync();
+        }
 
         /// <summary>
         /// Gets the bitmap in the clipboard.
         /// </summary>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        Task<Stream> GetBitmapAsync();
+        public virtual async Task<Stream> GetBitmapAsync()
+        {
+            var clipboard = Clipboard.GetContent();
+
+            var bitmap = await clipboard.GetBitmapAsync();
+
+            var stream = await bitmap.OpenReadAsync();
+
+            return stream.AsStream();
+        }
 
         /// <summary>
         /// Sets the current text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The text content that is stored in the clipboard.</param>
-        void SetText(string content);
+        public virtual void SetText(string content)
+        {
+            var package = new DataPackage();
+
+            package.SetText(content);
+
+            Clipboard.SetContent(package);
+        }
 
         /// <summary>
         /// Sets the current web link content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The web link content that is stored in the clipboard.</param>
-        void SetWebLink(Uri content);
+        public virtual void SetWebLink(Uri content)
+        {
+            var package = new DataPackage();
+
+            package.SetWebLink(content);
+
+            Clipboard.SetContent(package);
+        }
 
         /// <summary>
         /// Sets the current application link content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">application link content that is stored in the clipboard.</param>
-        void SetApplicationLink(Uri content);
+        public virtual void SetApplicationLink(Uri content)
+        {
+            var package = new DataPackage();
+
+            package.SetApplicationLink(content);
+
+            Clipboard.SetContent(package);
+        }
 
         /// <summary>
         /// Sets the current RTF text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The RTF text content that is stored in the clipboard.</param>
-        void SetRtf(string content);
+        public virtual void SetRtf(string content)
+        {
+            var package = new DataPackage();
+
+            package.SetRtf(content);
+
+            Clipboard.SetContent(package);
+        }
 
         /// <summary>
         /// Sets the current HTML text content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The HTML text content that is stored in the clipboard.</param>
-        void SetHtml(string content);
+        public virtual void SetHtml(string content)
+        {
+            var package = new DataPackage();
+
+            package.SetHtmlFormat(content);
+
+            Clipboard.SetContent(package);
+        }
 
         /// <summary>
         /// Sets the current bitmap content that is stored in the clipboard.
         /// </summary>
         /// <param name="content">The bitmap content that is stored in the clipboard.</param>
-        void SetBitmap(Stream content);
+        public virtual void SetBitmap(Stream content)
+        {
+            var package = new DataPackage();
+
+            var random = content.AsRandomAccessStream();
+
+            var reference = RandomAccessStreamReference.CreateFromStream(random);
+
+            package.SetBitmap(reference);
+        }
+
+        private static bool IsTypeInClipboard(string format)
+        {
+            var content = Clipboard.GetContent();
+
+            return content.Contains(format);
+        }
     }
 }
