271527f5aa1464d66e8995d4d0ab767047507d49
Pedro Lamas <pedrolamas@gmail.com>
2015-11-30 20:39:35 +0000

    Bumped the version to 2.1.0

--- build/default.ps1
+++ build/default.ps1
@@ -10,7 +10,7 @@ properties {
   
   $isAppVeyor = Test-Path -Path env:\APPVEYOR
   
-  $version = "2.0.0"
+  $version = "2.1.0"
   
   $tempDir = "$binDir\temp"
   $binariesDir = "$binDir\binaries"
--- src/Cimbalino.Toolkit (UWP)/Properties/AssemblyInfo.cs
+++ src/Cimbalino.Toolkit (UWP)/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 
 [assembly: ComVisible(false)]
