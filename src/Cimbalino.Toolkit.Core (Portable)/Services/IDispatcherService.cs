52bbacb1fd18585d245878c4b99705367933fcca
Pedro Lamas <pedrolamas@gmail.com>
2015-12-13 21:57:57 +0000

    Added the IDispatcherService interface and implementation

---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/IDispatcherService.cs
@@ -0,0 +1,89 @@
+﻿// ****************************************************************************
+// <copyright file="IDispatcherService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Core</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Threading.Tasks;
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of handling the UI thread dispatcher.
+    /// </summary>
+    public interface IDispatcherService
+    {
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="action">The action to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task InvokeOnUiThreadAsync(Action action);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="action">The action to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task InvokeOnUiThreadAsync(Action action, bool force);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="function">The function to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<T> InvokeOnUiThreadAsync<T>(Func<T> function);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="function">The function to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<T> InvokeOnUiThreadAsync<T>(Func<T> function, bool force);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="asyncAction">The async action to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task InvokeOnUiThreadAsync(Func<Task> asyncAction);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="asyncAction">The async action to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task InvokeOnUiThreadAsync(Func<Task> asyncAction, bool force);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="asyncFunction">The async function to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<T> InvokeOnUiThreadAsync<T>(Func<Task<T>> asyncFunction);
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="asyncFunction">The async function to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<T> InvokeOnUiThreadAsync<T>(Func<Task<T>> asyncFunction, bool force);
+    }
+}
---
+++ src/Cimbalino.Toolkit.Core (WP8)/Services/DispatcherService.cs
@@ -0,0 +1,162 @@
+﻿// ****************************************************************************
+// <copyright file="DispatcherService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Core</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Extensions;
+using Windows.UI.Core;
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="IDispatcherService"/>.
+    /// </summary>
+    public class DispatcherService : IDispatcherService
+    {
+        private readonly CoreDispatcher _dispatcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DispatcherService"/> class.
+        /// </summary>
+        public DispatcherService()
+        {
+            var coreWindow = CoreWindow.GetForCurrentThread();
+
+            if (coreWindow != null)
+            {
+                _dispatcher = coreWindow.Dispatcher;
+            }
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="action">The action to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task InvokeOnUiThreadAsync(Action action)
+        {
+            return this.InvokeOnUiThreadAsync(action, false);
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="action">The action to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task InvokeOnUiThreadAsync(Action action, bool force)
+        {
+            if (_dispatcher != null && (force || !_dispatcher.HasThreadAccess))
+            {
+                return _dispatcher.RunAndAwaitAsync(CoreDispatcherPriority.Normal, action);
+            }
+            else
+            {
+                action();
+
+                return Task.FromResult(0);
+            }
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="function">The function to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task<T> InvokeOnUiThreadAsync<T>(Func<T> function)
+        {
+            return this.InvokeOnUiThreadAsync(function, false);
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="function">The function to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task<T> InvokeOnUiThreadAsync<T>(Func<T> function, bool force)
+        {
+            if (_dispatcher != null && (force || !_dispatcher.HasThreadAccess))
+            {
+                return _dispatcher.RunAndAwaitAsync(CoreDispatcherPriority.Normal, function);
+            }
+            else
+            {
+                var result = function();
+
+                return Task.FromResult(result);
+            }
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="asyncAction">The async action to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task InvokeOnUiThreadAsync(Func<Task> asyncAction)
+        {
+            return this.InvokeOnUiThreadAsync(asyncAction, false);
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <param name="asyncAction">The async action to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task InvokeOnUiThreadAsync(Func<Task> asyncAction, bool force)
+        {
+            if (_dispatcher != null)
+            {
+                return _dispatcher.RunAndAwaitAsync(CoreDispatcherPriority.Normal, asyncAction);
+            }
+            else
+            {
+                return asyncAction();
+            }
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="asyncFunction">The async function to call.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task<T> InvokeOnUiThreadAsync<T>(Func<Task<T>> asyncFunction)
+        {
+            return this.InvokeOnUiThreadAsync(asyncFunction, false);
+        }
+
+        /// <summary>
+        /// Runs the event dispatcher and awaits for it to complete before returning the results for dispatched events asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The return type.</typeparam>
+        /// <param name="asyncFunction">The async function to call.</param>
+        /// <param name="force">Force dispatcher invokation even if already on UI thread.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task<T> InvokeOnUiThreadAsync<T>(Func<Task<T>> asyncFunction, bool force)
+        {
+            if (_dispatcher != null)
+            {
+                return _dispatcher.RunAndAwaitAsync(CoreDispatcherPriority.Normal, asyncFunction);
+            }
+            else
+            {
+                return asyncFunction();
+            }
+        }
+    }
+}
