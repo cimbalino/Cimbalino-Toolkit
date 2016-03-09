ea368a127a77fa411347f28d1236885cd3ca9cc7
Scott Lovegrove <scottisafool@live.co.uk>
2016-03-09 16:14:07 +0000

    Added back key handler code

--- src/Cimbalino.Toolkit.Controls (UWP)/Controls/ExtendedPageBase.cs
+++ src/Cimbalino.Toolkit.Controls (UWP)/Controls/ExtendedPageBase.cs
@@ -3,19 +3,32 @@ using Windows.UI.Xaml.Navigation;
 using Cimbalino.Toolkit.Extensions;
 using Cimbalino.Toolkit.Navigation;
 using Windows.UI.Xaml.Controls;
+using Cimbalino.Toolkit.Services;
 #else
 using System.Windows.Navigation;
 using Cimbalino.Toolkit.Extensions;
 using Cimbalino.Toolkit.Navigation;
+using Cimbalino.Toolkit.Services;
 using Page = Microsoft.Phone.Controls.PhoneApplicationPage;
 #endif
 
 namespace Cimbalino.Toolkit.Controls
 {
-    public class ExtendedPageBase : Page
+    public abstract class ExtendedPageBase : Page
     {
+#if !WINDOWS_APP
+        public new abstract INavigationService NavigationService { get; }
+#endif
+
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
+#if !WINDOWS_APP
+            if (NavigationService != null)
+            {
+                NavigationService.BackKeyPressed -= OnBackKeyPressed;
+            }
+#endif
+
             var vm = DataContext as INavigatedFrom;
 #if WINDOWS_PHONE
             vm?.OnNavigatedFrom(e.NavigationMode.ToNavigationMode());
@@ -28,6 +41,13 @@ namespace Cimbalino.Toolkit.Controls
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+#if !WINDOWS_APP
+            if (NavigationService != null)
+            {
+                NavigationService.BackKeyPressed += OnBackKeyPressed;
+            }
+#endif
+
             var vm = DataContext as INavigatedTo;
 #if WINDOWS_PHONE
             vm?.OnNavigatedTo(e.NavigationMode.ToNavigationMode());
@@ -37,5 +57,11 @@ namespace Cimbalino.Toolkit.Controls
 
             base.OnNavigatedTo(e);
         }
+
+#if !WINDOWS_APP
+        protected virtual void OnBackKeyPressed(object sender, NavigationServiceBackKeyPressedEventArgs e)
+        {
+        }
+#endif
     }
 }
