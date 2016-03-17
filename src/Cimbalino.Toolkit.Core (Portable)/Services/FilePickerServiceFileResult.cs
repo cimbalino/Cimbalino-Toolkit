dd105e09c8e37ecf9a911c5a0fe276d0c3f1cd8f
Pedro Lamas <pedrolamas@gmail.com>
2016-03-17 11:22:01 +0000

    Further improvements and refactoring of the IFilePickerService interface and implementations

---
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/FilePickerServiceFileResult.cs
@@ -0,0 +1,84 @@
+﻿// ****************************************************************************
+// <copyright file="FilePickerServiceFileResult.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Core</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if PORTABLE
+using System;
+using System.IO;
+using System.Threading.Tasks;
+#elif WINDOWS_PHONE
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+#else
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+#endif
+
+namespace Cimbalino.Toolkit.Services
+{
+    /// <summary>
+    /// Represents the result of the <see cref="IFilePickerService.PickSingleFileAsync"/> operation.
+    /// </summary>
+#if !NETFX_CORE
+    [CLSCompliant(false)]
+#endif
+    public class FilePickerServiceFileResult
+    {
+#if !PORTABLE
+        private readonly StorageFile _storageFile;
+#endif
+
+        /// <summary>
+        /// Gets the file name.
+        /// </summary>
+        /// <value>The file name.</value>
+        public string FileName
+        {
+            get
+            {
+#if PORTABLE
+                return null;
+#else
+                return _storageFile.Name;
+#endif
+            }
+        }
+
+#if !PORTABLE
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilePickerServiceFileResult" /> class.
+        /// </summary>
+        /// <param name="storageFile">The selected <see cref="StorageFile"/>.</param>
+        public FilePickerServiceFileResult(StorageFile storageFile)
+        {
+            _storageFile = storageFile;
+        }
+#endif
+
+        /// <summary>
+        /// Retrieves a stream for reading the selected file.
+        /// </summary>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public Task<Stream> OpenStreamForReadAsync()
+        {
+#if PORTABLE
+            return Task.FromResult((Stream)null);
+#else
+            return _storageFile.OpenStreamForReadAsync();
+#endif
+        }
+    }
+}
--- src/Cimbalino.Toolkit.Core (Portable)/Services/IFilePickerService.cs
+++ src/Cimbalino.Toolkit.Core (Portable)/Services/IFilePickerService.cs
@@ -1,25 +1,48 @@
-﻿using System.Collections.Generic;
+﻿// ****************************************************************************
+// <copyright file="IFilePickerService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2014
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <project>Cimbalino.Toolkit.Core</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if !NETFX_CORE
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+#else
+using System.Collections.Generic;
+using System.Threading.Tasks;
+#endif
 
 namespace Cimbalino.Toolkit.Services
 {
     /// <summary>
-    /// Interface for file pickers
+    /// Represents a service capable of handling the file pickers.
     /// </summary>
+#if !NETFX_CORE
+    [CLSCompliant(false)]
+#endif
     public interface IFilePickerService
     {
         /// <summary>
-        /// Opens the single file picker asynchronous.
+        /// Shows the file picker so that the user can pick one file.
         /// </summary>
         /// <param name="options">The options.</param>
-        /// <returns>A FilePickerResult</returns>
-        Task<FilePickerResult> OpenSingleFilePickerAsync(FilePickerOptions options);
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<FilePickerServiceFileResult> PickSingleFileAsync(FilePickerServiceOptions options);
 
         /// <summary>
-        /// Opens the multiple files picker asynchronous.
+        /// Shows the file picker so that the user can pick multiple files.
         /// </summary>
         /// <param name="options">The options.</param>
-        /// <returns>A list of FilePickerResult objects</returns>
-        Task<List<FilePickerResult>> OpenMultipleFilesPickerAsync(FilePickerOptions options);
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        Task<IEnumerable<FilePickerServiceFileResult>> PickMultipleFilesAsync(FilePickerServiceOptions options);
     }
 }
