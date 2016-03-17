841cff0e3d242613f01e611583499fe7ecc531c4
Pedro Lamas <pedrolamas@gmail.com>
2016-03-17 14:53:57 +0000

    Made FilePickerService unavailable for WP8 as the API isn't supported

--- src/Cimbalino.Toolkit (WP8)/Services/FilePickerService.cs
+++ src/Cimbalino.Toolkit (WP8)/Services/FilePickerService.cs
@@ -12,31 +12,17 @@
 // </license>
 // ****************************************************************************
 
-#if WINDOWS_PHONE || WINDOWS_APP || WINDOWS_UWP
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Windows.Storage.Pickers;
-#elif WINDOWS_PHONE_APP
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Windows.ApplicationModel.Activation;
-using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-#endif
+using Cimbalino.Toolkit.Helpers;
 
 namespace Cimbalino.Toolkit.Services
 {
     /// <summary>
     /// Represents an implementation of the <see cref="IFilePickerService"/>.
     /// </summary>
-#if !NETFX_CORE
     [CLSCompliant(false)]
-#endif
     public class FilePickerService : IFilePickerService
     {
         /// <summary>
@@ -44,49 +30,9 @@ namespace Cimbalino.Toolkit.Services
         /// </summary>
         /// <param name="options">The options.</param>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        public virtual async Task<FilePickerServiceFileResult> PickSingleFileAsync(FilePickerServiceOptions options)
+        public virtual Task<FilePickerServiceFileResult> PickSingleFileAsync(FilePickerServiceOptions options)
         {
-            var filePicker = CreateFileOpenPicker(options);
-
-#if WINDOWS_PHONE || WINDOWS_UWP || WINDOWS_APP
-            var selectedFile = await filePicker.PickSingleFileAsync();
-#elif WINDOWS_PHONE_APP
-            var coreApplicationView = CoreApplication.GetCurrentView();
-            var taskCompletionSource = new TaskCompletionSource<StorageFile>();
-
-            TypedEventHandler<CoreApplicationView, IActivatedEventArgs> handler = null;
-
-            handler = (sender, e) =>
-            {
-                coreApplicationView.Activated -= handler;
-
-                var fileOpenPickerContinuationEventArgs = e as FileOpenPickerContinuationEventArgs;
-
-                if (fileOpenPickerContinuationEventArgs != null &&
-                    fileOpenPickerContinuationEventArgs.Kind == ActivationKind.PickFileContinuation &&
-                    fileOpenPickerContinuationEventArgs.Files != null)
-                {
-                    taskCompletionSource.SetResult(fileOpenPickerContinuationEventArgs.Files.FirstOrDefault());
-                }
-                else
-                {
-                    taskCompletionSource.SetResult(null);
-                }
-            };
-
-            coreApplicationView.Activated += handler;
-
-            filePicker.PickSingleFileAndContinue();
-
-            var selectedFile = await taskCompletionSource.Task.ConfigureAwait(false);
-#endif
-
-            if (selectedFile != null)
-            {
-                return new FilePickerServiceFileResult(selectedFile);
-            }
-
-            return null;
+            return ExceptionHelper.ThrowNotSupported<Task<FilePickerServiceFileResult>>();
         }
 
         /// <summary>
@@ -94,67 +40,9 @@ namespace Cimbalino.Toolkit.Services
         /// </summary>
         /// <param name="options">The options.</param>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-        public virtual async Task<IEnumerable<FilePickerServiceFileResult>> PickMultipleFilesAsync(FilePickerServiceOptions options)
+        public virtual Task<IEnumerable<FilePickerServiceFileResult>> PickMultipleFilesAsync(FilePickerServiceOptions options)
         {
-            var filePicker = CreateFileOpenPicker(options);
-
-#if WINDOWS_PHONE || WINDOWS_UWP || WINDOWS_APP
-            var selectedFiles = await filePicker.PickMultipleFilesAsync();
-#elif WINDOWS_PHONE_APP
-            var coreApplicationView = CoreApplication.GetCurrentView();
-            var taskCompletionSource = new TaskCompletionSource<IReadOnlyList<StorageFile>>();
-
-            TypedEventHandler<CoreApplicationView, IActivatedEventArgs> handler = null;
-
-            handler = (sender, e) =>
-            {
-                coreApplicationView.Activated -= handler;
-
-                var fileOpenPickerContinuationEventArgs = e as FileOpenPickerContinuationEventArgs;
-
-                if (fileOpenPickerContinuationEventArgs != null &&
-                    fileOpenPickerContinuationEventArgs.Kind == ActivationKind.PickFileContinuation &&
-                    fileOpenPickerContinuationEventArgs.Files != null)
-                {
-                    taskCompletionSource.SetResult(fileOpenPickerContinuationEventArgs.Files);
-                }
-                else
-                {
-                    taskCompletionSource.SetResult(null);
-                }
-            };
-
-            coreApplicationView.Activated += handler;
-
-            filePicker.PickMultipleFilesAndContinue();
-
-            var selectedFiles = await taskCompletionSource.Task.ConfigureAwait(false);
-#endif
-            if (selectedFiles != null && selectedFiles.Count > 0)
-            {
-                return selectedFiles.Select(x => new FilePickerServiceFileResult(x));
-            }
-
-            return null;
-        }
-
-        private FileOpenPicker CreateFileOpenPicker(FilePickerServiceOptions options)
-        {
-            var fileOpenPicker = new FileOpenPicker
-            {
-                SuggestedStartLocation = options.SuggestedStartLocation.ToPickerLocationId(),
-                ViewMode = options.ViewMode.ToPickerViewMode()
-            };
-
-            if (options.FileTypeFilters != null)
-            {
-                foreach (var fileTypeFilter in options.FileTypeFilters)
-                {
-                    fileOpenPicker.FileTypeFilter.Add(fileTypeFilter);
-                }
-            }
-
-            return fileOpenPicker;
+            return ExceptionHelper.ThrowNotSupported<Task<IEnumerable<FilePickerServiceFileResult>>>();
         }
     }
 }
