669a4ffba82f00625d471d2fb24681fd7282bcd0
Pedro Lamas <pedrolamas@gmail.com>
2016-03-21 21:03:22 +0000

    Improvements on Install.ps1 script for WP81

--- build/Cimbalino.Toolkit/tools/wp81/Install.ps1
+++ build/Cimbalino.Toolkit/tools/wp81/Install.ps1
@@ -1,20 +1,24 @@
 param($installPath, $toolsPath, $package, $project)
   Write-Host "Adding System.Windows.Interactivity..."
   
-  if (Test-Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\BlendPhoneSDK") {
-    $blendSdkPath = Get-ItemPropertyValue "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\Windows Phone" -Name "(default)"
-  }
-  elseif (Test-Path "HKLM:\SOFTWARE\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\BlendPhoneSDK") {
-    $blendSdkPath = Get-ItemPropertyValue "HKLM:\SOFTWARE\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\Windows Phone" -Name "(default)"
+  $knownRegistryPaths = @(
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\BlendPhoneSDK",
+    "HKLM:\SOFTWARE\Microsoft\Microsoft SDKs\WindowsPhone\v8.0\AssemblyFoldersEx\BlendPhoneSDK"
+  )
+  
+  $validRegistryPath = $knownRegistryPaths | ? { Test-Path $_ } | Select-Object -First 1
+  
+  if ($validRegistryPath) {
+    $blendSdkPath = (Get-ItemProperty $validRegistryPath -Name "(default)")."(default)"
   }
   
   $blendSdkAssemblyPath = "$blendSdkPath\System.Windows.Interactivity.dll"
   
   if (Test-Path $blendSdkAssemblyPath) {
     $project.Object.References.Add($blendSdkAssemblyPath)
+    
+    $project.Save()
   }
   else {
     Write-Host "Unable to find System.Windows.Interactivity, please add it manually!"
   }
-  
-  $project.Save()
