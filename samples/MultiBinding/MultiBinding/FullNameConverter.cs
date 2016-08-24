9b858d2d6ab53eef4feff8f70b6bf1e8ffb03c15
Pedro Lamas <pedrolamas@gmail.com>
2016-08-24 22:15:06 +0100

    Added MultiBinding sample

---
+++ samples/MultiBinding/MultiBinding/FullNameConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Cimbalino.Toolkit.Converters;
+
+namespace MultiBinding
+{
+    public class FullNameConverter : MultiValueConverterBase
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null)
+            {
+                return null;
+            }
+
+            return string.Join(" ", values);
+        }
+
+        public override object[] ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = (string)value + " ";
+
+            var values = stringValue.Split(' ');
+
+            return new object[] { values[0], values[1] };
+        }
+    }
+}
