bfe61924c1e3867451957f928ddee4c32c4e7b78
Pedro Lamas <pedrolamas@gmail.com>
2016-10-28 11:02:14 +0100

    Added CancellationToken support to a few services

--- src/Cimbalino.Toolkit (WP8)/Services/MessageBoxService.cs
+++ src/Cimbalino.Toolkit (WP8)/Services/MessageBoxService.cs
@@ -14,12 +14,14 @@
 
 #if WINDOWS_PHONE || WINDOWS_PHONE_81
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Xna.Framework.GamerServices;
 #else
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
 #endif
@@ -38,6 +40,17 @@ namespace Cimbalino.Toolkit.Services
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public virtual Task ShowAsync(string text)
         {
+            return ShowAsync(text, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Displays a message box that contains the specified text and an OK button.
+        /// </summary>
+        /// <param name="text">The message to display.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public virtual Task ShowAsync(string text, CancellationToken cancellationToken)
+        {
 #if WINDOWS_PHONE || WINDOWS_PHONE_81
             MessageBox.Show(text);
 
@@ -45,7 +58,8 @@ namespace Cimbalino.Toolkit.Services
 #else
             var message = new MessageDialog(text);
 
-            return message.ShowAsync().AsTask();
+            return message.ShowAsync()
+                .AsTask(cancellationToken);
 #endif
         }
 
@@ -57,6 +71,18 @@ namespace Cimbalino.Toolkit.Services
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
         public virtual Task ShowAsync(string text, string caption)
         {
+            return ShowAsync(text, caption, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Displays a message box that contains the specified text, title bar caption, and an OK button.
+        /// </summary>
+        /// <param name="text">The message to display.</param>
+        /// <param name="caption">The title of the message box.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+        public virtual Task ShowAsync(string text, string caption, CancellationToken cancellationToken)
+        {
 #if WINDOWS_PHONE || WINDOWS_PHONE_81
             MessageBox.Show(text, caption, MessageBoxButton.OK);
 
@@ -64,7 +90,8 @@ namespace Cimbalino.Toolkit.Services
 #else
             var message = new MessageDialog(text, caption);
 
-            return message.ShowAsync().AsTask();
+            return message.ShowAsync()
+                .AsTask(cancellationToken);
 #endif
         }
 
@@ -75,8 +102,21 @@ namespace Cimbalino.Toolkit.Services
         /// <param name="caption">The title of the message box.</param>
         /// <param name="buttons">The captions for message box buttons. The maximum number of buttons is two.</param>
         /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
-#if WINDOWS_PHONE || WINDOWS_PHONE_81
         public virtual Task<int> ShowAsync(string text, string caption, IEnumerable<string> buttons)
+        {
+            return ShowAsync(text, caption, buttons, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Displays a message box that contains the specified text, title bar caption, and response buttons.
+        /// </summary>
+        /// <param name="text">The message to display.</param>
+        /// <param name="caption">The title of the message box.</param>
+        /// <param name="buttons">The captions for message box buttons. The maximum number of buttons is two.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        /// <returns>The <see cref="Task"/> object representing the asynchronous operation.</returns>
+#if WINDOWS_PHONE || WINDOWS_PHONE_81
+        public virtual Task<int> ShowAsync(string text, string caption, IEnumerable<string> buttons, CancellationToken cancellationToken)
         {
             var taskCompletionSource = new TaskCompletionSource<int>();
 
@@ -90,7 +130,7 @@ namespace Cimbalino.Toolkit.Services
             return taskCompletionSource.Task;
         }
 #else
-        public virtual async Task<int> ShowAsync(string text, string caption, IEnumerable<string> buttons)
+        public virtual async Task<int> ShowAsync(string text, string caption, IEnumerable<string> buttons, CancellationToken cancellationToken)
         {
             var message = new MessageDialog(text, caption);
 
@@ -99,7 +139,8 @@ namespace Cimbalino.Toolkit.Services
                 message.Commands.Add(new UICommand(button));
             }
 
-            var command = await message.ShowAsync();
+            var command = await message.ShowAsync()
+                .AsTask(cancellationToken);
 
             if (command != null)
             {
